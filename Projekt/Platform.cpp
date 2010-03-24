0d6442e8351aaa6ce96000d1216f03faacb5ad0c
U-RYB-520FB58EE61\ryba <ryba@ryb-520fb58ee61.(none)>
2010-03-24 16:14:48 +0100

    poprawianie klasy Platform, ktora zostala wrzucona przez Mariusza z masa bledow ...

--- Projekt/Platform.cpp
+++ Projekt/Platform.cpp
@@ -1,19 +1,23 @@
 #include <allegro.h>
 #include "Platform.h"
 
-void Platform::Platform(int _x, int _y, int _w, int _h)
+Platform::Platform(int _x, int _y, int _w, int _h) :
+	x(_x),
+	y(_y),
+	w(_w),
+	h(_h)
 {
-	x = _x;
-	y = _y;
-	w = _w;
-	h = _h;
 }
 void Platform::setBitmap(char *bgPath)
 {
-	platform = load_bmp(bgPath,default_palette); //ladowanie grafiki
-	if (!platform) //sprawdzamy czy sie zaladowala bitmapka
+	gfx = load_bmp(bgPath, default_palette); //ladowanie grafiki
+	if (!gfx) //sprawdzamy czy zaladowala sie bitmapka
 	{
 		set_gfx_mode(GFX_TEXT,0,0,0,0);
-		allegro_message("Nie mogê za³adowaæ grafiki platformy!");
+		allegro_message("Nie moge zaladowac grafiki tla platformy!");
 	}
 }
+int Platform::getX() { return x; }
+int Platform::getY() { return y; }
+int Platform::getW() { return w; }
+int Platform::getH() { return h; }
--- Projekt/Platform.h
+++ Projekt/Platform.h
@@ -5,15 +5,16 @@
 #define PLATFORM_H
 
 class Platform {
-	int x, y, w, h;
-	BITMAP *platforma = NULL; //deklaracja bitmapy (wskaznik na nia)
 public:
 	Platform(int _x, int _y, int _w, int _h);
-	setBitmap(char *bgPath);
-	int getX(){return x;}
-	int getY(){return y;}
-	int getW(){return w;}
-	int getH(){return h;}
+	void setBitmap(char *bgPath);
+	int getX();
+	int getY();
+	int getW();
+	int getH();
+private:
+	int x, y, w, h;
+	BITMAP *gfx; //deklaracja bitmapy (wskaznik na nia)
 };
 
 #endif
